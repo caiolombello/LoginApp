0e4a95419178cde8c9df8e70bcd8fae13d78cfd6
caiol <caiolvbarbieri@pm.me>
2022-01-09 15:08:43 -0300

    Repo recreated

---
+++ src/main/java/com/ebank/login/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.ebank.login.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/ebank/login/registration/RegistrationService.java
@@ -0,0 +1,33 @@
+package com.ebank.login.registration;
+
+import com.ebank.login.appuser.AppUser;
+import com.ebank.login.appuser.AppUserRole;
+import com.ebank.login.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RegistrationService {
+
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
+    public String register(RegistrationRequest request) {
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+
+        if (!isValidEmail) {
+            throw new IllegalStateException("Email not valid");
+        }
+
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
+    }
+}
